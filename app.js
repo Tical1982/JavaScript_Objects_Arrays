6811ed9fd20208c6a27c041ec0595bcd0ae303aa
Johnathan Robinson <johnathan.l.robinson@gmail.com>
2025-08-28 19:48:57 -0500

    completed exercise 1 and 2 , working on 3

--- app.js
+++ app.js
@@ -7,3 +7,69 @@ console.log("EXERCISE 1:\n==========\n");
 console.log("EXERCISE 2:\n==========\n");
 
 
+//let total = 0;
+    // for ( let i = 0; i  numbers.length; i++ ) { {
+    //   total += " numbers  [i]"
+    // }
+    //   return total;
+
+    // }
+
+function arraySum(numbers) {
+    let sum = 0; 
+    
+    numbers.forEach((number) => {
+        sum += number; 
+    });
+
+    return sum;
+    
+}
+
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+console.log(arraySum(numbers)); // Output: 239
+
+
+const book = {};
+  book.title = " The Hobbit";
+  book.author = "J.R.R. Tolken";
+  book.pages = 295;
+  book.readCount = 1;
+
+  book.info = function()  {
+       return `${this.title} by ${this.author} is
+       ${this.pages} pages, read status: ${this.readCount} time${this.readCount > 1 ? 's' : ''}.`;
+  }
+
+
+console.log(book.info());
+
+
+
+// let sentence = "The quick brown fox jumps over the lazy dog";
+// let words = sentence.split(" ");
+// let reversedWords = words.map( word => word.split(""). reverse().join(""));
+// let reversdSentence  = reversedWords.join("");
+// console.log(reversedSentence);
+
+
+
+// let csvData = "name , age \\nFrodo,50\\nSam,38\\nMerry,36\\nPippin,26";
+
+// let rows = csvData.split('\\n');
+// let headers = rows [0] . spliter(',');
+// let result = [];
+
+// for (let i = 1; i  rows.length; i++) {
+//   let dataValues = rows[i]. split(',');
+//   let newObject = {};
+//   for (let j = 0; j  headers.length; j ++) {
+// newObject[headers[j]] = isNaN(dataValues[j]) ?
+// dataValues[j] : parseInt(dataValues[j]);
+// }
+// result.push(newObject);
+
+//   }
+
+//   console.log(result);
